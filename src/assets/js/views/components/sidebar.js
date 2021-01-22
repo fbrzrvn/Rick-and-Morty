c9f28a96e2c668ddcafd91929a789ebc0885e61d
fabrizio <faber.rvn@gmail.com>
2021-01-22 18:05:11 +0100

    #3 Get response and populate the sidebar

--- src/assets/js/views/components/sidebar.js
+++ src/assets/js/views/components/sidebar.js
@@ -1,7 +1,22 @@
-export const sidebar = () =>{
+export const sidebar = () => {
   const template =
     `
-      <aside class="sidebar"></aside>
-    `
+      <aside class="sidebar">
+        <ul class="sidebar-list" data-episodes-list></ul>
+        <button class="sidebar-btn">Load More Episode</button>
+      </aside>
+    `;
   return template;
 }
+
+export const sidebarLink = data => {
+  const fragment = $(document.createDocumentFragment());
+  data.forEach(element => {
+    const item = $('<li class="sidebar-item"></li>');
+    const link = $(`<a href="${element.url} class="sidebar-link"></a>`);
+    link.text(`Episode: ${element.id}`);
+    item.append(link)
+    $(fragment).append(item);
+  });
+  return fragment;
+};
