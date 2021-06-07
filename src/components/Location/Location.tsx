5c8719f0b0bd69e4491fdb4d35fc07617908dfe4
Fabrizio Ervini <faber.rvn@gmail.com>
2021-06-07 11:50:31 +0200

    feat: add location card

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,33 +1,59 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { fetchAll, fetchApi } from '../../api';
+import getEndpoint from '../../helpers/getEndPoint';
+import {
+  CardBtn,
+  CardDetails,
+  CardH2,
+  CardHeader,
+  CardP,
+  CardWrap,
+  Wrapper,
+  WrapperCard,
+  WrapperImg,
+  WrapperLink,
+} from './styles';
 
 const Location = () => {
   const location = useLocation();
+  const history = useHistory();
   const endPoint: string = location.pathname;
   const [data, setData] = useState<any>([]);
-  const [char, setChar] = useState<any>([]);
+  const [residents, setResidents] = useState<any>([]);
 
   useEffect(() => {
     fetchApi(endPoint).then((response: ApiDataType | any) => {
       setData(response.data);
       fetchAll(response.data?.residents).then((response: ApiDataType | any) =>
-        setChar(response)
+        setResidents(response)
       );
     });
   }, [endPoint]);
 
   return (
-    <div>
-      <p>{data.name}</p>
-      <p>{data.dimension}</p>
-      <p>{data.type}</p>
-      {char.map((el: any) => (
-        <div key={el.data.id} className="card">
-          <p>{el.data.name}</p>
-        </div>
-      ))}
-    </div>
+    <CardWrap>
+      <CardHeader>
+        <CardH2>{data.name}</CardH2>
+        <CardDetails>
+          <CardP>{data.dimension}</CardP>
+          <CardP>|</CardP>
+          <CardP>{data.type}</CardP>
+        </CardDetails>
+      </CardHeader>
+      <CardH2>Residents:</CardH2>
+      <Wrapper>
+        {residents.map((resident: any) => (
+          <WrapperCard key={resident.data.id}>
+            <WrapperImg src={resident.data.image} alt={resident.data.name} />
+            <WrapperLink to={`/${getEndpoint(resident.data.url)}`}>
+              {resident.data.name}
+            </WrapperLink>
+          </WrapperCard>
+        ))}
+      </Wrapper>
+      <CardBtn onClick={() => history.goBack()}>Go Back</CardBtn>
+    </CardWrap>
   );
 };
 
